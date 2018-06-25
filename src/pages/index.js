26d4e900781af6e7e7ab8399ef944d364eb9750d
Mark Ridgway <mjridgw@gmail.com>
2018-06-25 16:54:07 -0600

    project query working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,26 @@
 import React, { Component } from 'react'
-import Link from 'gatsby-link'
-import styled from 'styled-components';
-
-const Project = styled.div`
-  margin-bottom: 20px;
-  padding-bottom: 20px;
-  border-bottom: 1px solid #ccc;
-
-  &:first-of-type{
-    margin-top: 20px;
-    padding-top: 20px;
-    border-top: 1px solid #ccc;
-  }
-`;
-
-const ProjectTitle = styled.h2`
-  margin-bottom: 1em;
-`;
-
-const ProjectImage = styled.div`
-  margin-bottom: 1em;
-
-  img{
-    max-width: 100%;
-  }
-`;
+import ProjectCard from '../components/project-card';
 
 // class IndexPage extends Component{
-const IndexPage = ({ data }) => (
-  <div>
-    <h1>Past Projects</h1>
-    { displayProjects(data.allMarkdownRemark.edges) }
-  </div>
-);
+class IndexPage extends Component {
+  render(){
+    const projects = this.props.data.allMarkdownRemark.edges;
 
-function displayProjects(projects){
-  return projects.map( (project) => {
-    return (
-      <Project key={ project.node.frontmatter.title.replace(' ', '-').toLowerCase() }>
-        <h2>{ project.node.frontmatter.title }</h2>
-        <ProjectImage>
-          <img src={ project.node.frontmatter.projectImage.publicURL } />
-        </ProjectImage>
-      </Project>
+    return(
+      <div>
+        { projects.map( (project) => (
+          <ProjectCard
+            key={ project.node.frontmatter.title.replace(' ', '-')}
+            title={ project.node.frontmatter.title }
+            image={ project.node.frontmatter.projectImage.publicURL } />
+        ))}
+      </div>
     )
-  });
+  }
 }
 
 export const query = graphql`
-  query IndexQuery {
+  query ProjectsQuery {
     allMarkdownRemark(
       filter: { fileAbsolutePath: {regex : "\/content/projects/"} },
       sort: {fields: [frontmatter___order], order: ASC}
