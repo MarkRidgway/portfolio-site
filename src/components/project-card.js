4434e892ea2bfc3e1f4cbf7fd3cd243c2c92af5b
Mark Ridgway <mjridgw@gmail.com>
2018-06-25 16:54:07 -0600

    project query working

---
+++ src/components/project-card.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components';
+
+const Project = styled.div``;
+
+const ProjectTitle = styled.h2`
+  margin-bottom: 1em;
+`;
+
+const ProjectImage = styled.div`
+  margin-bottom: 1em;
+
+  img{
+    max-width: 100%;
+  }
+`;
+
+class ProjectCard extends Component {
+  render(){
+    return(
+      <Project>
+        <ProjectTitle>{ this.props.title }</ProjectTitle>
+        <ProjectImage>
+          <img src={ this.props.image } />
+        </ProjectImage>
+      </Project>
+    )
+  }
+}
+
+export default ProjectCard
